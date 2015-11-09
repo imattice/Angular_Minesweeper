fc58d14da456207020c8b13ec5fa445c155c0166
Ike Mattice <gogreen37@me.com>
2015-11-09 14:49:55 -0800

    add single test mine.

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,40 @@
 var minesweeper = angular.module('minesweeper', []);
 
 
-function createMinefield() {
-    var minefield = {};
-    minefield.rows = [];
+//create gameboard
+    function createMinefield() {
+        var minefield = {};
+        minefield.rows = [];
 
-    //creates each row in the minefield
-    for(var i=0; i<9; i++){
-        var row = {};
-        row.spots = [];
+        //creates each row in the minefield
+        for(var i=0; i<9; i++){
+            var row = {};
+            row.spots = [];
 
-        //each row has 9 spots that are not revealed
-        for(var j=0; j<9; j++){
-            var spot = {};
-            spot.isCovered = true;
-            row.spots.push(spot);
+            //each row has 9 spots that are not revealed
+            for(var j=0; j<9; j++){
+                var spot = {};
+                spot.isCovered = true;
+                spot.content = 'empty';
+                row.spots.push(spot);
+            }
+            minefield.rows.push(row);
         }
-
-        minefield.rows.push(row);
+        placeRandomMine(minefield);
+        return minefield;
     }
-    return minefield;
-}
+
+//finds a specific spot
+    function getSpot(minefield, row, column){
+        return minefield.rows[row].spots[column];
+    }
+
+//places mines
+    function placeRandomMine(minefield) {
+        //create mines
+        var row = Math.round(Math.random() * 8);
+        var column = Math.round(Math.random() * 8);
+
+        var spot = getSpot(minefield, row, column);
+        spot.content = 'mine';
+    }
